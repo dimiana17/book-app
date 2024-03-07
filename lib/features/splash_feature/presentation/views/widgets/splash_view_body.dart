04c9e4266031cff6173df0c668bd26f102e13ac6
dimiana17 <93286131+dimiana17@users.noreply.github.com>
2024-03-07 19:55:09 +0200

    add animation

--- lib/features/splash_feature/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash_feature/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,28 @@
 import 'package:book_app/core/utls/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin{
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(vsync: this,duration: const Duration(seconds: 2));
+    slidingAnimation=Tween<Offset>(begin:const Offset(0, 2) ,end: const Offset(0, 0)).animate(animationController);
+    animationController.forward();
+    slidingAnimation.addListener(() { 
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,12 +32,15 @@ class SplashViewBody extends StatelessWidget {
       children: [
         Image.asset(AssetsData.logo),
         const SizedBox(height: 4,),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold
+         SlideTransition(
+          position: slidingAnimation,
+          child: const Text(
+            'Read Free Books',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold
+            ),
           ),
         )
       ],
