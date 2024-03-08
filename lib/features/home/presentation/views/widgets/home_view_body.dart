a9e8f3d099821a946f44562306caab8e7b82633d
dimiana17 <93286131+dimiana17@users.noreply.github.com>
2024-03-08 18:51:11 +0200

    best seller list view item p1

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,8 +1,10 @@
+import 'package:book_app/constants.dart';
 import 'package:book_app/core/styles.dart';
 import 'package:book_app/core/utls/assets.dart';
 import 'package:book_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:book_app/features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:book_app/features/home/presentation/views/widgets/featured_box_list_view.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -18,8 +20,45 @@ class HomeViewBody extends StatelessWidget {
         children: [
           CustomAppBar(),
           FeaturedBoxListView(),
-          SizedBox(height: 50,),
-          Text("Best Seller",style: Styles.titleMediem,)
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            "Best Seller",
+            style: Styles.titleMediem,
+          ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(16),
+                image: const DecorationImage(
+                  image: AssetImage(AssetsData.test,),
+                  fit: BoxFit.fill
+                ),
+              ),
+            ),
+          ),
+          const Column(
+            children: [],
+          )
         ],
       ),
     );
