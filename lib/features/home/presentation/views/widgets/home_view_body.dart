be0316e7a92131d14a1f67c2d7aee13d559745e0
dimiana17 <93286131+dimiana17@users.noreply.github.com>
2024-03-13 12:27:11 +0200

    best seller item p3

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:book_app/core/styles.dart';
 import 'package:book_app/core/utls/assets.dart';
+import 'package:book_app/features/home/presentation/views/widgets/best_seller_item.dart';
 import 'package:book_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:book_app/features/home/presentation/views/widgets/featured_box_list_view.dart';
 import 'package:flutter/material.dart';
@@ -33,46 +34,3 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.circular(16),
-                image: const DecorationImage(
-                    image: AssetImage(
-                      AssetsData.test,
-                    ),
-                    fit: BoxFit.fill),
-              ),
-            ),
-          ),
-          const SizedBox(
-            width: 30,
-          ),
-          Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width*.5,
-                  child: const Text(
-                'Harry Potter and the Goblet of Fire',
-                style: Styles.textStyle20,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ))
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
