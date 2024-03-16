1896a520d4c125646d5ab148d86de79e2115a116
dimiana17 <93286131+dimiana17@users.noreply.github.com>
2024-03-16 14:07:57 +0200

    create books action

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -2,6 +2,8 @@ import 'package:book_app/core/styles.dart';
 import 'package:book_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:book_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:book_app/features/home/presentation/views/widgets/custom_book_item.dart';
+import 'package:book_app/widgets/custom_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -34,13 +36,50 @@ class BookDetailsViewBody extends StatelessWidget {
             opacity: .7,
             child: Text(
               'Rodyard Kipling',
-              style: Styles.textStyle18.copyWith(fontWeight: FontWeight.w500,fontStyle: FontStyle.italic),
+              style: Styles.textStyle18.copyWith(
+                  fontWeight: FontWeight.w500, fontStyle: FontStyle.italic),
             ),
           ),
           const SizedBox(
             height: 18,
           ),
-          const BookRating()
+          const BookRating(),
+          const SizedBox(
+            height: 30,
+          ),
+          const BooksAction()
+        ],
+      ),
+    );
+  }
+}
+
+class BooksAction extends StatelessWidget {
+  const BooksAction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      child: Row(
+        children: [
+          Expanded(
+              child: CustomButton(
+            backgroungColor: Colors.white,
+            textColor: Colors.black,
+            borderRedius: BorderRadius.only(
+                topLeft: Radius.circular(16), bottomLeft: Radius.circular(16)),
+            text: '19.99\$',
+          )),
+          Expanded(
+              child: CustomButton(
+            backgroungColor: Color(0xFFE87866),
+            textColor: Colors.white,
+            borderRedius: BorderRadius.only(
+                topRight: Radius.circular(16),
+                bottomRight: Radius.circular(16)),
+            text: 'Free Preview',
+          ))
         ],
       ),
     );
---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,31 @@
+import 'package:book_app/core/styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.backgroungColor, required this.textColor, this.borderRedius, required this.text});
+    final Color backgroungColor;
+    final Color textColor;
+    final BorderRadius? borderRedius;
+    final String text;
+  @override
+  Widget build(BuildContext context) {
+
+    return SizedBox(
+      height: 48,
+      child: TextButton(
+        style: TextButton.styleFrom(
+            shape:
+                RoundedRectangleBorder(borderRadius:borderRedius?? BorderRadius.circular(16)),
+            backgroundColor: backgroungColor),
+        onPressed: () {},
+        child: Text(
+          text,
+          style: Styles.textStyle18.copyWith(
+            color: textColor,
+            fontWeight: FontWeight.w900
+          ),
+        ),
+      ),
+    );
+  }
+}
