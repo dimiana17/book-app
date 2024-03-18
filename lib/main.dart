6d0ddc9fa0971839ccb5fbb6b3937f0f6bec5398
dimiana17 <93286131+dimiana17@users.noreply.github.com>
2024-03-18 15:20:34 +0200

    create book model

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:book_app/constants.dart';
 import 'package:book_app/core/utls/app_router.dart';
-import 'package:book_app/features/splash_feature/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
